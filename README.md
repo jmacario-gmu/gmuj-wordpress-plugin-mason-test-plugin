0f03d2d92666c01fa90531ed2da1d744c91ba89e
Jan Macario <jmacario@GMU.EDU>
2021-01-26 13:03:00 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+Plugin name: Mason Test Plugin
+
+Link to GitHub repository: [https://github.com/jmacario-gmu/gmuj-wordpress-plugin-mason-test-plugin](https://github.com/jmacario-gmu/gmuj-wordpress-plugin-mason-test-plugin)
+
+Summary: Mason WordPress plugin used for testing purposes.
+
+Description:  
+
---
+++ gmuj-wordpress-plugin-mason-test-plugin.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Main plugin file for the Mason Test plugin
+ */
+
+/**
+ * Plugin Name:       Mason WordPress: Mason Test Plugin
+ * Author:            Jan Macario
+ * Plugin URI:        https://github.com/jmacario-gmu/gmuj-wordpress-plugin-mason-test-plugin
+ * Description:       Mason WordPress plugin used for testing purposes.
+ * Version:           1.0.0
+ */
+
+
+// Exit if this file is not called directly.
+	if (!defined('WPINC')) {
+		die;
+	}
+
+
