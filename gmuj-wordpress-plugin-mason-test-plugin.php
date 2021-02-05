5269a7c89c727221ae5ddb148744eeecfd31c461
Jan Macario <jmacario@GMU.EDU>
2021-02-05 13:26:02 -0500

    Updates to version 1.4, but I'm going to name the tag with a lower version number.

--- gmuj-wordpress-plugin-mason-test-plugin.php
+++ gmuj-wordpress-plugin-mason-test-plugin.php
@@ -9,10 +9,10 @@
  * Author:            Jan Macario
  * Plugin URI:        https://github.com/jmacario-gmu/gmuj-wordpress-plugin-mason-test-plugin
  * Description:       Mason WordPress plugin used for testing purposes.
- * Version:           1.0.3
+ * Version:           1.0.4
  */
 
-// This is an unimportant change. This is supposed to be version 1.0.3, but I'm going to forget to modify the version number and commit the change and tag it. Will the auto update process make the update because it's a new tag, or will it not because it thinks the version is the same?
+// Now I'm going to see if the version number specified in the git tag has any effect on the auto-updates. So far I have been naming tags using the version number specified in the plugin, so the version number and the tag name _should_ always be identical. But based on my last test I don't think that is required. This time I'm going to try naming the tag with a _lower_ version number than the version number specified in the theme, to see if that causes any problem with the auto-updates. If it does not, I can conclude that the tag name is not relevant to the auto-update process.
 
 // Exit if this file is not called directly.
 	if (!defined('WPINC')) {
